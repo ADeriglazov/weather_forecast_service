5d861fc8345dd12eccf61ec01a4b3c1e29484f88
AD <aleksander@MacBook-Pro-Aleksandr.local>
2020-08-13 14:37:24 +0300

    Output format fix & pom.xml fix

--- src/main/java/org/ad/forecast/builder/ForecastBuilder.java
+++ src/main/java/org/ad/forecast/builder/ForecastBuilder.java
@@ -4,13 +4,13 @@ import org.ad.forecast.WeatherForecastElement;
 
 public class ForecastBuilder {
 
-    private static final String MINSK_FORECAST_FORMAT = "Сегодня в Минске от %.2f до %.2f °C. " +
-            "Текущая температура %.2f°C (ощущается как %.2f°C).\n " +
+    private static final String MINSK_FORECAST_FORMAT = "Сегодня в Минске от %.0f до %.0f °C. " +
+            "Текущая температура %.0f°C (ощущается как %.0f°C).\n " +
             "Берегите себя и своих близких!";
 
-    private static final String COMMON_FORECAST_FORMAT = "Сегодня в городе %s от %.2f до %.2f °C. " +
-            "Текущая температура %.2f°C, но по ощущениям %.2f°C.\n " +
-            "Для сравнения в столице Республики Беларусь %.2f°C.\n " +
+    private static final String COMMON_FORECAST_FORMAT = "Сегодня в городе %s от %.0f до %.0f °C. " +
+            "Текущая температура %.0f°C, но по ощущениям %.0f°C.\n " +
+            "Для сравнения в столице Республики Беларусь %.0f°C.\n " +
             "Берегите себя и своих близких!";
 
     public static String build(WeatherForecastElement element) {
