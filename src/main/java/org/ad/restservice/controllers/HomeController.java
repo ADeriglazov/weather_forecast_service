ae2ec957c7b4bc0acc1035aba9f25cd7577efa90
AD <aleksander@MacBook-Pro-Aleksandr.local>
2020-08-13 21:31:40 +0300

    refactoring commit

--- src/main/java/org/ad/restservice/controllers/HomeController.java
+++ src/main/java/org/ad/restservice/controllers/HomeController.java
@@ -4,23 +4,26 @@ package org.ad.restservice.controllers;
 import org.ad.request.handler.OWMWeatherForecastRequestHandlerImpl;
 import org.ad.request.handler.PageNotFoundException;
 import org.ad.request.handler.WeatherForecastRequestHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.RequestContextHolder;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedDeque;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Controller
 public class HomeController {
 
-    private final ConcurrentHashMap<String, ConcurrentLinkedDeque<String>> counter = new ConcurrentHashMap();
+    private static final Logger logger = LogManager.getLogger(HomeController.class);
+
+    private final ConcurrentHashMap<String, ConcurrentLinkedDeque<String>> sessions =
+            new ConcurrentHashMap<>();
 
     @PostMapping(value = "/")
     public String greetingPost(@RequestParam(value = "city", defaultValue = "Москва") String city)
@@ -32,17 +35,17 @@ public class HomeController {
         try {
             response = requestHandler.getResponse(city);
         } catch (PageNotFoundException e) {
-            System.out.println(String.format("WARN: Handled PageNotFoundException. See the code below:\n%s", e.getMessage()));
+            logger.warn(String.format("Handled PageNotFoundException. See the code below:%n%s", e.getMessage()));
             response = String.format("Запрос погоды в городе %s не может быть выполнен, приносим свои извенения. " +
                     "Возможно, ошибка в написании. Проверьте и попробуйте еще раз!", city);
         } catch (IOException e) {
-            System.out.println(String.format("WARN: Handled IOException. While parsing:\n%s", e.getMessage()));
+            logger.warn(String.format("Handled IOException. While parsing:%n%s", e.getMessage()));
             response = String.format("Запрос погоды в городе %s не может быть выполнен, приносим свои извенения. " +
                     "Попробуйте мгновение спустя!", city);
         }
-        ConcurrentLinkedDeque<String> queryStack2Session = counter.getOrDefault(sessionId, new ConcurrentLinkedDeque<>());
+        ConcurrentLinkedDeque<String> queryStack2Session = sessions.getOrDefault(sessionId, new ConcurrentLinkedDeque<>());
         queryStack2Session.addFirst(response);
-        counter.put(sessionId, queryStack2Session);
+        sessions.put(sessionId, queryStack2Session);
 
         return "redirect:/";
     }
@@ -50,7 +53,7 @@ public class HomeController {
     @GetMapping(value = "/")
     public String greetingGet(Model model) {
         String sessionId = RequestContextHolder.currentRequestAttributes().getSessionId();
-        List<String> historyStack = new LinkedList(counter.getOrDefault(sessionId, new ConcurrentLinkedDeque()));
+        List<String> historyStack = new LinkedList<>(sessions.getOrDefault(sessionId, new ConcurrentLinkedDeque<>()));
         model.addAttribute("messages",historyStack);
         return "weather-view";
     }
